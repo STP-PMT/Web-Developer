ef576a91a4d6b9bff4d9d258707292888c6fbe53
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2020-12-28 09:40:31 +0700

    Lab0502

---
+++ Web-Developer/Lab0502/index.php
@@ -0,0 +1,46 @@
+<?php
+    $servername  ="localhost";
+    $username ="demo";
+    $password ="abc123";
+    $dbname ="demo";
+
+    $conn = new mysqli($servername,$username,$password,$dbname);
+    if($conn-> connect_error){
+        die("Connection Fail".$conn->connect_error);
+    }else{
+        echo "Connection success <br>";
+    }
+
+    $sql ="select * from employees";
+    $result = $conn->query($sql);
+
+    if($result->num_rows>0){
+        ?>
+        <style>table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+        </style>
+
+            <table>
+            <tr>
+                <td>Firstname</td>
+                <td>Lastname</td>
+            </tr>
+        <?php
+        while($row = $result->fetch_assoc()){
+            ?>
+                <tr>
+                <td><?=$row["firstName"]?></td>
+                <td><?=$row["lastName"]?></td>
+                </tr>
+            <?php
+        }
+        ?>
+        <br>
+        </table>
+        <?php
+    }else{
+        echo "No result";
+    }
+?>
