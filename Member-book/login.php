879abc030d80ace3a54a0d7b968638300b41dbe3
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2021-01-14 23:06:09 +0700

    Member_book

--- Member-book/login.php
+++ Member-book/login.php
@@ -1,11 +1,12 @@
 <?php
 class dbconnect
 {
-    public $servername = 'localhost';
-    public $username = 'demo';
-    public $password = '123456';
-    public $dbname = 'demo';
+    public $servername = '202.28.34.197';
+    public $username = 'stu62011212109';
+    public $password = '62011212109@csmsu';
+    public $dbname = 'stu62011212109';
     public $conn;
+    public $pass;
 
     public function db_start()
     {
@@ -19,20 +20,30 @@ class dbconnect
 
     public function login($username, $password)
     {
-        $stmt = $this->conn->prepare("select * from employees where email=?");
-        $stmt->bind_param("s", $username);
+        $stmt = $this->conn->prepare("select * from admin where username=?");
+        $stmt->bind_param('s', $username);
         $stmt->execute();
 
         $result = $stmt->get_result();
         if ($result->num_rows == 1) {
             $row = $result->fetch_assoc();
-            include 'home.php';
-            $this->console_log("Login Success");
+            $this->pass = $row['password'];
         } else {
-            include 'index.html';
+            $this->pass =  "";
+        }
+
+        if (password_verify($password, $this->pass)) {
+            include 'memberbook.php';
+        } else {
             echo "<script>alert('ชื่อผู้ใช้หรือรหัสผ่านผิด');</script>";
+            include 'index.html';
             $this->console_log("Login Faile");
         }
+    }
+
+    public function show()
+    {
+        echo "Hello,world";
     }
 
     public function console_log($output, $with_script_tags = true)
@@ -48,5 +59,9 @@ class dbconnect
 
 $db = new dbconnect();
 $db->db_start();
-$db->login($_POST['user'], $_POST['pass']);
+if ($_POST['Login'] == 'noLogin') {
+    include 'memberbook.php';
+} else if ($_POST['Login'] == 'Login') {
+    $db->login($_POST['user'], $_POST['pass']);
+}
 $db->conn->close();
