58b0822c7205972b3ac7a4908a2e97fdcc511622
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2021-01-25 16:14:59 +0700

    แก้ code ที่ซ้ำ

--- web-service/api/products.php
+++ web-service/api/products.php
@@ -88,31 +88,22 @@ $app->get('/products', function (Request $request, Response $response, $args) {
 $app->get('/products/{seacrh_field}/{keyword}', function (Request $request, Response $response, $args) {
     $field = $args['seacrh_field'];
     $key = $args['keyword'];
+    $condb = $GLOBALS['conn'];
     if ($field == 'productCode') {
-        $condb = $GLOBALS['conn'];
         $stmt = $condb->prepare('select * from products where productCode=?');
-        $stmt->bind_param('s', $key);
-        $stmt->execute();
-        $result =  $stmt->get_result();
-        $data = array();
-        while ($row = $result->fetch_assoc()) {
-            array_push($data, $row);
-        }
-        $json = json_encode($data);
-        $response->getBody()->write($json);
     } else if ($field == 'productName') {
-        $condb = $GLOBALS['conn'];
         $stmt = $condb->prepare('select * from products where productName=?');
-        $stmt->bind_param('s', $key);
-        $stmt->execute();
-        $result =  $stmt->get_result();
-        $data = array();
-        while ($row = $result->fetch_assoc()) {
-            array_push($data, $row);
-        }
-        $json = json_encode($data);
-        $response->getBody()->write($json);
     }
+
+    $stmt->bind_param('s', $key);
+    $stmt->execute();
+    $result =  $stmt->get_result();
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        array_push($data, $row);
+    }
+    $json = json_encode($data);
+    $response->getBody()->write($json);
 
     return $response->withHeader('content-Type', 'application/json');
 });
