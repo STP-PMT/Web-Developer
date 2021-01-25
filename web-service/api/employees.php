49fe6f8577432a5777fb90e82d9f8e1f522ea3c2
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2021-01-25 11:31:55 +0700

    done all

--- web-service/api/employees.php
+++ web-service/api/employees.php
@@ -30,3 +30,28 @@ $app->post('/employees', function (Request $request, Response $response, $args) 
     
     return $response->withHeader('content-Type', 'application/json');
 });
+
+$app->post('/employees/new', function (Request $request, Response $response, $args) {
+    $body = $request->getBody();
+    $bodyArray = json_decode($body, true);
+    
+    $conn = $GLOBALS['conn'];
+    $stmt = $conn->prepare("select password,employeeNumber from employees where email=?");
+    $stmt->bind_param("s",$bodyArray['email']);
+    $stmt->execute();
+    $result=$stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    if(password_verify($bodyArray['password'],$row['password'])){
+        $conn = $GLOBALS['conn'];
+        $hash=password_hash($bodyArray['newPassword'],PASSWORD_DEFAULT);
+        $stmt = $conn->prepare("update employees set password=? where employeeNumber=?");
+        $stmt->bind_param("ss",$hash,$row['employeeNumber']);
+        $stmt->execute();
+        $response->getBody()->write('update done');
+    }else{
+        $response->getBody()->write($bodyArray['password']);
+    }
+    
+    return $response->withHeader('content-Type', 'application/json');
+});
