c2df8c67b4b30927f96b06f24421c036de00df4e
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2021-01-25 10:41:44 +0700

    login done

--- web-service/api/employees.php
+++ web-service/api/employees.php
@@ -3,26 +3,30 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-$app->get('/employees/insert', function (Request $request, Response $response, $args) {
+$app->post('/employees', function (Request $request, Response $response, $args) {
+    $body = $request->getBody();
+    $bodyArray = json_decode($body, true);
+    $conn = $GLOBALS['conn'];
+    $stmt = $conn->prepare("select password from employees where email=?");
+    $stmt->bind_param("s",$bodyArray['email']);
+    $stmt->execute();
+    $result=$stmt->get_result();
+    $row = $result->fetch_assoc();
 
-    $response->getBody()->write("Employees insert");
-    return $response;
-});
-
-$app->get('/employees/update', function (Request $request, Response $response, $args) {
-
-    $response->getBody()->write("Employees update");
-    return $response;
-});
-
-$app->get('/employees/delete', function (Request $request, Response $response, $args) {
-
-    $response->getBody()->write("Employees delete");
-    return $response;
-});
-
-$app->get('/employees/seacrh', function (Request $request, Response $response, $args) {
-
-    $response->getBody()->write("Employees seacrh");
-    return $response;
+    if(password_verify($bodyArray['password'],$row['password'])){
+        $stmt = $conn->prepare("select * from employees where email=?");
+        $stmt->bind_param("s",$bodyArray['email']);
+        $stmt->execute();
+        $result=$stmt->get_result();
+        $data = array();
+        while($row = $result->fetch_assoc()){
+            array_push($data,$row);
+        }
+        $json = json_encode($data);
+        $response->getBody()->write($json);
+    }else{
+        echo 'Login Fail';
+    }
+    
+    return $response->withHeader('content-Type', 'application/json');
 });
