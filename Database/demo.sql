e02d9fcc7d7388b4112d4a06ae9d13607bb95daa
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2021-01-28 13:42:48 +0700

    เปลี่ยนชื่อ Database demo

--- Database/demo.sql
+++ Database/demo.sql
@@ -2,7 +2,7 @@
 *********************************************************************
 http://www.mysqltutorial.org
 *********************************************************************
-Name: MySQL Sample Database stu62011212109
+Name: MySQL Sample Database demo
 Link: http://www.mysqltutorial.org/mysql-sample-database.aspx
 Version 3.1
 + changed data type from DOUBLE to DECIMAL for amount columns
@@ -23,9 +23,9 @@ Version 2.0
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`stu62011212109` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`demo` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `stu62011212109`;
+USE `demo`;
 
 /*Table structure for table `customers` */
 
