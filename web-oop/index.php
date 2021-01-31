4320448d76a9907b578442328ddcd782108bf966
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2021-01-31 21:34:06 +0700

    login form

--- web-oop/index.php
+++ web-oop/index.php
@@ -1,19 +1,26 @@
 <html>
 
 <head>
-    <?php include 'products.php';
-    $pd = new products();
-    ?>
-    <a href="bootstrap/css/bootstrap.min.css"></a>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div>
-
+    <div class="container" style="width: 400px">
+        <div class="card">
+            <div class="container">
+                <h1>Login</h1>
+                <div class="form-group">
+                    <label for="">email</label>
+                    <input type="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">password</label>
+                    <input type="password" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="submit"class="btn btn-success">
+                </div>
             </div>
-            <?=$pd->show();?>
         </div>
     </div>
     <script src="bootstrap/js/bootstrap.min.js"></script>
