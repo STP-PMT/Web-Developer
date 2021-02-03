539e19f85e660c64a5e2846c69236d084df5239c
STP-PMT <62597051+STP-PMT@users.noreply.github.com>
2021-02-03 15:02:32 +0700

    time error

--- web-thaichana/server.php
+++ web-thaichana/server.php
@@ -1,25 +1,28 @@
 <?php
+date_default_timezone_set('Asia/Bangkok');
 session_start();
 $servername = 'localhost';
 $username = 'thaichana';
 $password = '123456';
 $dbname = 'thaichana';
-$date = date("Y-m-d") . " " . date('H:i:s', time() - date('Z') + 010700);
+
+// $time = date('H:i:s', time() - date('Z') + 010700);
+// $date = date_create(date("Y-m-d"));
+$date = date("d-m-Y") . " " . date('H:i:s');
+$dates =date("d-m-Y") . " " . date('H:i:s',time()-date('H:i:s')+75*60);
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 $stmt = $conn->prepare('select * from place');
 $stmt->execute();
 $place = $stmt->get_result();
 
-if(isset($_GET['checkin'])){
-    echo 1;
+if (isset($_GET['checkin'])) {
     $phone = $_GET['phoneno'];
-    if(is_numeric($phone) and strlen($phone) == 10){
-        $_SESSION["msg"]='1';
-    }else{
-        $_SESSION["msg"]='2';
+    if (is_numeric($phone) and strlen($phone) == 10) {
+        $_SESSION["msg"] = '1';
+        echo $date.'<br>'.$dates ;
+    } else {
+        $_SESSION["msg"] = '2';
     }
-    header('location:index.php');
+    // header('location:index.php');
 }
-
-?>
